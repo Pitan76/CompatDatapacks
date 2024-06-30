9a25f5cd828f5b9c09c1f2efd25045640e9b3dce
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-06-30 15:47:00 +0900

    prepare env

--- forge/src/main/java/net/pitan76/compatdatapacks/forge/CompatDatapacksForge.java
+++ forge/src/main/java/net/pitan76/compatdatapacks/forge/CompatDatapacksForge.java
@@ -1,6 +1,6 @@
 package net.pitan76.compatdatapacks.forge;
 
-import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.pitan76.compatdatapacks.CompatDatapacks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -8,8 +8,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(CompatDatapacks.MOD_ID)
 public class CompatDatapacksForge {
     public CompatDatapacksForge() {
-		// Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(CompatDatapacks.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onSetup);
+    }
+
+    public void onSetup(FMLCommonSetupEvent e) {
         CompatDatapacks.init();
     }
 }
--- neoforge/src/main/java/net/pitan76/compatdatapacks/neoforge/CompatDatapacksNeoForge.java
+++ neoforge/src/main/java/net/pitan76/compatdatapacks/neoforge/CompatDatapacksNeoForge.java
@@ -1,15 +1,17 @@
 package net.pitan76.compatdatapacks.neoforge;
 
-import dev.architectury.platform.forge.EventBuses;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.pitan76.compatdatapacks.CompatDatapacks;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(CompatDatapacks.MOD_ID)
 public class CompatDatapacksNeoForge {
-    public CompatDatapacksNeoForge() {
-		// Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(CompatDatapacks.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+    public CompatDatapacksNeoForge(IEventBus bus) {
+        bus.addListener(this::onSetup);
+    }
+
+    public void onSetup(FMLCommonSetupEvent e) {
         CompatDatapacks.init();
     }
 }
