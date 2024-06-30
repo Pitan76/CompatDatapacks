d90693fa3cf458ca845cc9b4117d6a65e856c3f9
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-06-30 16:07:18 +0900

    add phpscript

--- phpscript/build.bat
+++ phpscript/build.bat
@@ -5,8 +5,15 @@ set modid=compatdatapacks76
 git fetch origin
 timeout /T 1
 call gradlew.bat build
-call gradlew.bat generatePomFileForMavenJavaPublication
-copy /y ".\build\publications\mavenJava\pom-default.xml" ".\build\publications\mavenJava\%modid%.pom"
+call gradlew.bat generatePomFileForMavenCommonPublication
+call gradlew.bat generatePomFileForMavenFabricPublication
+rem call gradlew.bat generatePomFileForMavenForgePublication
+call gradlew.bat generatePomFileForMavenNeoForgePublication
+
+copy /y ".\build\publications\mavenCommon\pom-default.xml" ".\build\publications\mavenCommon\%modid%.pom"
+copy /y ".\build\publications\mavenFabric\pom-default.xml" ".\build\publications\mavenFabric\%modid%.pom"
+rem copy /y ".\build\publications\mavenForge\pom-default.xml" ".\build\publications\mavenForge\%modid%.pom"
+copy /y ".\build\publications\mavenNeoForge\pom-default.xml" ".\build\publications\mavenNeoForge\%modid%.pom"
 
 timeout /T 1
 
