721f56703a195ccd1ed534b950fab45a6cf77490
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-07-01 21:43:28 +0900

    support mineable

--- common/src/main/java/net/pitan76/compatdatapacks/mixin/TagGroupLoaderMixin.java
+++ common/src/main/java/net/pitan76/compatdatapacks/mixin/TagGroupLoaderMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.resource.ResourceFinder;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import net.pitan76.compatdatapacks.CompatDatapacks;
+import net.pitan76.compatdatapacks.OldMineablePaths;
 import net.pitan76.compatdatapacks.OldTags;
 import net.pitan76.compatdatapacks.RewriteLogs;
 import org.spongepowered.asm.mixin.Final;
@@ -38,13 +39,35 @@ public class TagGroupLoaderMixin {
 
         for (var oldTag : oldTags) {
             ResourceFinder resourceFinder = ResourceFinder.json(oldTag);
-            entries.addAll(resourceFinder.findAllResources(resourceManager).entrySet());
+            for (Map.Entry<Identifier, List<Resource>> entry : resourceFinder.findAllResources(resourceManager).entrySet()) {
+
+                if (fixMineable(entry, entries)) continue;
+
+                // add entry
+                entries.add(entry);
+            }
+
             ++RewriteLogs.loadingOldTags;
         }
 
         CompatDatapacks.log("Loaded old tags " + String.join(", ", oldTags) + " for " + dataType);
-
         return entries.iterator();
+    }
 
+    private static boolean fixMineable(Map.Entry<Identifier, List<Resource>> oldEntry, List<Map.Entry<Identifier, List<Resource>>> entries) {
+        var oldKey = oldEntry.getKey().toString();
+        if (!OldMineablePaths.contains(oldKey)) return false;
+
+        var newPath = OldMineablePaths.get(oldKey);
+        if (newPath.isEmpty()) return false;
+
+        for (Map.Entry<Identifier, List<Resource>> entry : entries) {
+            if (entry.getKey().toString().equalsIgnoreCase(newPath)) {
+                entry.getValue().addAll(oldEntry.getValue());
+                break;
+            }
+        }
+
+        return true;
     }
 }
