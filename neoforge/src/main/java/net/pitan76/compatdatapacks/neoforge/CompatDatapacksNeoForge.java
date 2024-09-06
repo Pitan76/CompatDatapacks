c7ac6d093e702640922a7f5d26ee3a3eb698f61f
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-06 15:19:16 +0900

    neoforge

--- neoforge/src/main/java/net/pitan76/compatdatapacks/neoforge/CompatDatapacksNeoForge.java
+++ neoforge/src/main/java/net/pitan76/compatdatapacks/neoforge/CompatDatapacksNeoForge.java
@@ -2,16 +2,11 @@ package net.pitan76.compatdatapacks.neoforge;
 
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.pitan76.compatdatapacks.CompatDatapacks;
 
 @Mod(CompatDatapacks.MOD_ID)
 public class CompatDatapacksNeoForge {
     public CompatDatapacksNeoForge(ModContainer modContainer) {
-        modContainer.getEventBus().addListener(this::onSetup);
-    }
-
-    public void onSetup(FMLCommonSetupEvent e) {
         CompatDatapacks.init();
     }
 }
