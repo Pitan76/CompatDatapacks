a0c4f714422b71e43464d72004f12d602b570799
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-28 22:19:52 +0900

    not working, but prepare

--- common/src/main/java/net/pitan76/compatdatapacks/mixin/TagGroupLoaderMixin.java
+++ common/src/main/java/net/pitan76/compatdatapacks/mixin/TagGroupLoaderMixin.java
@@ -6,13 +6,13 @@ import net.minecraft.resource.ResourceFinder;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import net.pitan76.compatdatapacks.CompatDatapacks;
+import net.pitan76.compatdatapacks.OldMineablePaths;
 import net.pitan76.compatdatapacks.OldTags;
 import net.pitan76.compatdatapacks.RewriteLogs;
-import net.pitan76.compatdatapacks.config.Config;
-import net.pitan76.compatdatapacks.config.IgnoreConfig;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Mixin(TagGroupLoader.class)
 public class TagGroupLoaderMixin {
@@ -28,28 +27,25 @@ public class TagGroupLoaderMixin {
 
     @ModifyVariable(method = "loadTags", at = @At("STORE"), ordinal = 0)
     private Iterator compatdatapacks76$modifyVar4(Iterator var4, ResourceManager resourceManager) {
-        if (!Config.isUseCompatTagGroup()) return var4;
-
         if (!OldTags.contains(dataType)) return var4;
 
         var oldTags = OldTags.get(dataType);
         if (oldTags == null || oldTags.isEmpty()) return var4;
 
         List<Map.Entry<Identifier, List<Resource>>> entries = new ArrayList<>();
-        while (var4.hasNext()) {
+        while(var4.hasNext()) {
             Map.Entry<Identifier, List<Resource>> entry = (Map.Entry)var4.next();
             entries.add(entry);
         }
 
-        Map<Identifier, List<Resource>> map = entries.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a));
-
         for (var oldTag : oldTags) {
             ResourceFinder resourceFinder = ResourceFinder.json(oldTag);
-            for (var entry : resourceFinder.findAllResources(resourceManager).entrySet()) {
-                var replaced = OldTags.replace(entry.getKey());
-                if (IgnoreConfig.contains(replaced.toString())) continue;
+            for (Map.Entry<Identifier, List<Resource>> entry : resourceFinder.findAllResources(resourceManager).entrySet()) {
 
-                entries.add(Map.entry(replaced, entry.getValue()));
+                if (compactdatapacks76$fixMineable(entry, entries)) continue;
+
+                // add entry
+                entries.add(entry);
             }
 
             ++RewriteLogs.loadingOldTags;
@@ -57,5 +53,23 @@ public class TagGroupLoaderMixin {
 
         CompatDatapacks.log("Loaded old tags " + String.join(", ", oldTags) + " for " + dataType);
         return entries.iterator();
+    }
+
+    @Unique
+    private static boolean compactdatapacks76$fixMineable(Map.Entry<Identifier, List<Resource>> oldEntry, List<Map.Entry<Identifier, List<Resource>>> entries) {
+        var oldKey = oldEntry.getKey().toString();
+        if (!OldMineablePaths.contains(oldKey)) return false;
+
+        var newPath = OldMineablePaths.get(oldKey);
+        if (newPath.isEmpty()) return false;
+
+        for (Map.Entry<Identifier, List<Resource>> entry : entries) {
+            if (entry.getKey().toString().equalsIgnoreCase(newPath)) {
+                entry.getValue().addAll(oldEntry.getValue());
+                break;
+            }
+        }
+
+        return true;
     }
 }
