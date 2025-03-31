5ffdbd86d3fc5410ac10b1cd56f13b9389fb3f07
PTOM76 <ptms76@outlook.jp>
2025-03-31 23:04:54 +0900

    containsKey

--- common/src/main/java/net/pitan76/compatdatapacks/config/Config.java
+++ common/src/main/java/net/pitan76/compatdatapacks/config/Config.java
@@ -86,6 +86,7 @@ public class Config {
     }
 
     public static boolean getBoolean(String key) {
+        if (!map.containsKey(key)) return false;
         return (boolean) map.get(key);
     }
 
